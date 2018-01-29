18a3ea40be0620ac2b3024d044b8416e6875b9a8
San Nguyen <san.nguyen@tanaka-scale.co.jp>
2018-01-29 10:08:49 +0900

    feat(components): add material components and shared module

--- apps/sand-admin/src/app/app.module.ts
+++ apps/sand-admin/src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MaterialModule } from '@sand-libs/material';
+import { SharedModule } from '@sand-libs/shared';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule.withServerTransition({ appId: 'sand-admin' }), BrowserAnimationsModule],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'sand-admin' }),
+    BrowserAnimationsModule,
+    MaterialModule,
+    SharedModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
