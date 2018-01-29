b69ccd218b293c20691429c34312ba04d33ac9f2
San Nguyen <san.nguyen@tanaka-scale.co.jp>
2018-01-29 10:58:28 +0900

    feat(sand-admin): add service-worker

--- apps/sand-admin/src/app/app.module.ts
+++ apps/sand-admin/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { MaterialModule } from '@sand-libs/material';
 import { SharedModule } from '@sand-libs/shared';
+import { environment } from '@sand-envs/environment';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -12,7 +14,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule.withServerTransition({ appId: 'sand-admin' }),
     BrowserAnimationsModule,
     MaterialModule,
-    SharedModule
+    SharedModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
