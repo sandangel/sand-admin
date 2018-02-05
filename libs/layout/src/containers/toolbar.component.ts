29bbf4ae5fc12d92c18988482d9c2eba5320e6df
sandangel <vinhsannguyen91@gmail.com>
2018-02-06 07:08:49 +0900

    feat(layout): add toolbar component

---
+++ libs/layout/src/containers/toolbar.component.ts
@@ -0,0 +1,30 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { LayoutState, createToggleSidenav } from '../+state';
+
+@Component({
+  selector: 'sand-toolbar',
+  template: `
+    <mat-toolbar class="mat-elevation-z2" color="primary">
+    	<button mat-button (click)="toggleSidenav()">
+    		<mat-icon>menu</mat-icon>
+    	</button>Sand Admin Template
+    </mat-toolbar>`,
+  styles: [
+    `
+    :host {
+        z-index: 2;
+    }
+    .mat-button {
+        margin-right: 10px;
+    }`
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ToolbarComponent {
+  constructor(private store: Store<LayoutState>) {}
+
+  toggleSidenav() {
+    this.store.dispatch(createToggleSidenav());
+  }
+}
