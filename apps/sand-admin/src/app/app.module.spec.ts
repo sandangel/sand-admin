e27744b4df125d6bbdd354136c1c70777f21896c
sandangel <vinhsannguyen91@gmail.com>
2018-02-07 21:40:18 +0900

    feat(app): config app module

--- apps/sand-admin/src/app/app.module.spec.ts
+++ apps/sand-admin/src/app/app.module.spec.ts
@@ -1,7 +1,25 @@
 import { AppModule } from './app.module';
+import { Store, StoreModule } from '@ngrx/store';
+import { NgrxSelect, NgrxUtilsModule } from '@ngrx-utils/store';
+import { TestBed } from '@angular/core/testing';
+import { routerReducer } from '@ngrx/router-store';
+import { REDUCER_TOKEN } from './app.module';
 
 describe('AppModule', () => {
+  let store: Store<any>;
+  let ngrxSelect: NgrxSelect;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [StoreModule.forRoot(REDUCER_TOKEN), NgrxUtilsModule],
+      providers: [{ provide: REDUCER_TOKEN, useFactory: () => ({ router: routerReducer }) }]
+    });
+
+    store = TestBed.get(Store);
+    ngrxSelect = TestBed.get(NgrxSelect);
+  });
+
   it('should be defined', () => {
-    expect(new AppModule()).toBeDefined();
+    expect(new AppModule(store, ngrxSelect)).toBeDefined();
   });
 });
--- apps/sand-admin/src/app/app.module.ts
+++ apps/sand-admin/src/app/app.module.ts
@@ -1,12 +1,54 @@
+import { NgModule, InjectionToken } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { EffectsModule } from '@ngrx/effects';
+import {
+  StoreRouterConnectingModule,
+  routerReducer,
+  RouterReducerState,
+  RouterStateSerializer
+} from '@ngrx/router-store';
+import { StoreModule, ActionReducerMap, Store } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '@sand-envs/environment';
+import { LayoutModule } from '@sand-libs/layout';
 import { MaterialModule } from '@sand-libs/material';
 import { SharedModule } from '@sand-libs/shared';
-import { environment } from '@sand-envs/environment';
+import { RouterModule, Params, RouterStateSnapshot } from '@angular/router';
+import { NgrxSelect, NgrxUtilsModule } from '@ngrx-utils/store';
 
+import { RouterEffects } from './+state/router.effects';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+export interface RouterStateUrl {
+  url: string;
+  params: Params;
+  queryParams: Params;
+}
+
+export interface State {
+  router: RouterReducerState<RouterStateUrl>;
+}
+
+export class CustomSerializer implements RouterStateSerializer<RouterStateUrl> {
+  serialize(routerState: RouterStateSnapshot): RouterStateUrl {
+    let route = routerState.root;
+
+    while (route.firstChild) {
+      route = route.firstChild;
+    }
+
+    const { url, root: { queryParams } } = routerState;
+    const { params } = route;
+
+    // Only return an object including the URL, params and query params
+    // instead of the entire snapshot
+    return { url, params, queryParams };
+  }
+}
+
+export const REDUCER_TOKEN = new InjectionToken<ActionReducerMap<State>>('Registered Reducers');
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,9 +57,25 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserAnimationsModule,
     MaterialModule,
     SharedModule,
-    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
+    RouterModule.forRoot([]),
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
+    StoreModule.forRoot(REDUCER_TOKEN),
+    EffectsModule.forRoot([RouterEffects]),
+    !environment.production ? StoreDevtoolsModule.instrument({ maxAge: 25 }) : [],
+    StoreRouterConnectingModule.forRoot({
+      stateKey: 'router'
+    }),
+    NgrxUtilsModule,
+    LayoutModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  providers: [
+    { provide: RouterStateSerializer, useClass: CustomSerializer },
+    { provide: REDUCER_TOKEN, useFactory: () => ({ router: routerReducer }) }
+  ]
 })
-export class AppModule {}
+export class AppModule {
+  constructor(store: Store<any>, ngrxSelect: NgrxSelect) {
+    ngrxSelect.connect(store);
+  }
+}
