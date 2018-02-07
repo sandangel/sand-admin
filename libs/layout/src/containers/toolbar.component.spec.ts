d557f630d32a400f9e04298d02a2a0888b7c6415
sandangel <vinhsannguyen91@gmail.com>
2018-02-07 21:44:27 +0900

    test(layout): config test module for toolbar component

--- libs/layout/src/containers/toolbar.component.spec.ts
+++ libs/layout/src/containers/toolbar.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { StoreModule } from '@ngrx/store';
+import { REDUCER_TOKEN } from '@sand-app/app.module';
 
 import { ToolbarComponent } from './toolbar.component';
+import { layoutReducer } from '../+state';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('ToolbarComponent', () => {
   let component: ToolbarComponent;
@@ -9,7 +13,10 @@ describe('ToolbarComponent', () => {
   beforeEach(
     async(() => {
       TestBed.configureTestingModule({
-        declarations: [ToolbarComponent]
+        declarations: [ToolbarComponent],
+        imports: [StoreModule.forRoot(REDUCER_TOKEN)],
+        providers: [{ provide: REDUCER_TOKEN, useFactory: () => ({ layout: layoutReducer }) }],
+        schemas: [NO_ERRORS_SCHEMA]
       }).compileComponents();
     })
   );
