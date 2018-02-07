a106456d2dbc62df824ae23e14f028b8a7acd39a
sandangel <vinhsannguyen91@gmail.com>
2018-02-07 21:34:55 +0900

    test(layout): test for layout component

--- libs/layout/src/containers/layout.component.spec.ts
+++ libs/layout/src/containers/layout.component.spec.ts
@@ -1,18 +1,26 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { LayoutComponent } from './layout.component';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { StoreModule, Store } from '@ngrx/store';
+import { REDUCER_TOKEN } from '@sand-app/app.module';
+
+import { layoutReducer, LayoutState } from '../+state';
+import { LayoutComponent } from './layout.component';
 
 describe('LayoutComponent', () => {
   let component: LayoutComponent;
   let fixture: ComponentFixture<LayoutComponent>;
+  let store: Store<LayoutState>;
 
   beforeEach(
     async(() => {
       TestBed.configureTestingModule({
         declarations: [LayoutComponent],
+        imports: [StoreModule.forRoot(REDUCER_TOKEN)],
+        providers: [{ provide: REDUCER_TOKEN, useFactory: () => ({ layout: layoutReducer }) }],
         schemas: [NO_ERRORS_SCHEMA]
       }).compileComponents();
+
+      store = TestBed.get(Store);
     })
   );
 
@@ -24,5 +32,9 @@ describe('LayoutComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should match snapshot', () => {
+    expect(component).toMatchSnapshot();
   });
 });
