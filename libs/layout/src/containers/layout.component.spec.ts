9f67f04faed0c44f1de31bc01b315153df6d58ad
sandangel <vinhsannguyen91@gmail.com>
2018-02-07 23:38:11 +0900

    test(layout): add test for OnPush layout

--- libs/layout/src/containers/layout.component.spec.ts
+++ libs/layout/src/containers/layout.component.spec.ts
@@ -1,40 +1,61 @@
-import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { NO_ERRORS_SCHEMA, Component } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { StoreModule, Store } from '@ngrx/store';
+import { StoreModule } from '@ngrx/store';
 import { REDUCER_TOKEN } from '@sand-app/app.module';
+import { MatSidenav } from '@angular/material';
 
-import { layoutReducer, LayoutState } from '../+state';
+import { layoutReducer } from '../+state';
 import { LayoutComponent } from './layout.component';
+import { MaterialModule } from '@sand-libs/material';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { By } from '@angular/platform-browser';
+
+@Component({
+  selector: 'sand-test',
+  template: `
+    <sand-layout [opened]="opened"></sand-layout>
+  `
+})
+class SandTestComponent {
+  opened: boolean;
+}
 
 describe('LayoutComponent', () => {
-  let component: LayoutComponent;
-  let fixture: ComponentFixture<LayoutComponent>;
-  let store: Store<LayoutState>;
+  let component: SandTestComponent;
+  let fixture: ComponentFixture<SandTestComponent>;
+  let sidenav: MatSidenav;
 
   beforeEach(
     async(() => {
       TestBed.configureTestingModule({
-        declarations: [LayoutComponent],
-        imports: [StoreModule.forRoot(REDUCER_TOKEN)],
+        declarations: [LayoutComponent, SandTestComponent],
+        imports: [StoreModule.forRoot(REDUCER_TOKEN), MaterialModule, NoopAnimationsModule],
         providers: [{ provide: REDUCER_TOKEN, useFactory: () => ({ layout: layoutReducer }) }],
         schemas: [NO_ERRORS_SCHEMA]
       }).compileComponents();
-
-      store = TestBed.get(Store);
     })
   );
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(LayoutComponent);
+    fixture = TestBed.createComponent(SandTestComponent);
     component = fixture.componentInstance;
+    sidenav = fixture.debugElement.query(By.directive(MatSidenav)).componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(sidenav).toBeDefined();
+    expect(sidenav).toBeInstanceOf(MatSidenav);
   });
 
-  it('should match snapshot', () => {
-    expect(component).toMatchSnapshot();
+  it('should change opened with @Input', () => {
+    component.opened = true;
+    fixture.detectChanges();
+    expect(sidenav.opened).toBe(true);
+
+    component.opened = false;
+    fixture.detectChanges();
+    expect(sidenav.opened).toBe(false);
   });
 });
