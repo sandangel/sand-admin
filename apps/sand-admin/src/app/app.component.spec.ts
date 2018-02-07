ac10cbb191fff58a51e4e4d4fcf9cbd4643e1671
sandangel <vinhsannguyen91@gmail.com>
2018-02-07 21:36:31 +0900

    feat(app): add basic layout for app component

--- apps/sand-admin/src/app/app.component.spec.ts
+++ apps/sand-admin/src/app/app.component.spec.ts
@@ -3,60 +3,34 @@
 import { async, TestBed, ComponentFixture } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
-import { NO_ERRORS_SCHEMA, DebugElement } from '@angular/core';
-import { By } from '@angular/platform-browser';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '@sand-libs/material';
+import { StoreModule } from '@ngrx/store';
+import { routerReducer } from '@ngrx/router-store';
+import { REDUCER_TOKEN } from './app.module';
 
 describe('AppComponent', () => {
   let fixture: ComponentFixture<AppComponent>;
-  let instance: AppComponent;
-  let de: DebugElement;
-  let el: HTMLElement;
 
   beforeEach(
     async(() => {
       TestBed.configureTestingModule({
         declarations: [AppComponent],
-        schemas: [NO_ERRORS_SCHEMA]
+        imports: [NoopAnimationsModule, MaterialModule, StoreModule.forRoot(REDUCER_TOKEN)],
+        schemas: [NO_ERRORS_SCHEMA],
+        providers: [{ provide: REDUCER_TOKEN, useFactory: () => ({ router: routerReducer }) }]
       }).compileComponents();
 
       fixture = TestBed.createComponent(AppComponent);
-      instance = fixture.componentInstance;
-      de = fixture.debugElement;
-      el = de.nativeElement;
     })
   );
-
-  it('should compile', () => {
-    expect(fixture).toMatchSnapshot();
-  });
-
-  it('should display right title if provied', () => {
-    instance.title = 'Hello';
-    fixture.detectChanges();
-    expect(el.textContent).toContain('Hello');
-    expect(fixture).toMatchSnapshot();
-  });
 
   it(
     'should create the app',
     async(() => {
       const app = fixture.debugElement.componentInstance;
       expect(app).toBeTruthy();
-    })
-  );
-  it(
-    `should have as title 'app'`,
-    async(() => {
-      const app = fixture.debugElement.componentInstance;
-      expect(app.title).toEqual('app');
-    })
-  );
-  it(
-    'should render title in a h1 tag',
-    async(() => {
-      fixture.detectChanges();
-      const compiled = fixture.debugElement.nativeElement;
-      expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
     })
   );
 });
--- apps/sand-admin/src/app/app.component.ts
+++ apps/sand-admin/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Pluck } from '@ngrx-utils/store';
+import { Observable } from 'rxjs/Observable';
+import { untilDestroy, pluck } from '@sand-libs/shared';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { LayoutState, createSetSidenav } from '@sand-libs/layout';
+import { Store } from '@ngrx/store';
 
 @Component({
-  selector: 'app-root',
+  selector: 'sand-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnDestroy {
+  @Pluck('layout.sidenavOpened') opened$: Observable<boolean>;
+
+  sidenavMode: 'over' | 'push' | 'side';
+
+  constructor(bpo: BreakpointObserver, store: Store<LayoutState>) {
+    bpo
+      .observe([Breakpoints.XSmall])
+      .pipe(pluck('matches'), untilDestroy(this))
+      .subscribe(
+        isSmallScreen =>
+          (this.sidenavMode = isSmallScreen
+            ? (store.dispatch(createSetSidenav(false)), 'over')
+            : (store.dispatch(createSetSidenav(true)), 'side'))
+      );
+  }
+
+  ngOnDestroy() {}
 }
